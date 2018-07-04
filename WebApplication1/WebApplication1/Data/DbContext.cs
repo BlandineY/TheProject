7890f345994b9eccc4d84575d040bfed64dd5923
Gary Vanwynsberghe <Adminl@EMER05>
2018-07-04 18:54:43 +0200

    Ajout preparation dbcontext

---
+++ WebApplication1/WebApplication1/Data/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public class DbContext : DbContext
+    {
+        public DbContext() : base("AJOUTER NOM BASE DE DONNEES") // Constructeur de la classe de base
+        {
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Migrations/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using TodoList.Data;
+
+namespace TodoList.Migrations
+{
+    public class Configuration : DbMigrationsConfiguration<DbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+    }
+}
