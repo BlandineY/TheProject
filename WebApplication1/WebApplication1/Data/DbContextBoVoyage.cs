eab519db6d0be29ccc11b410166903f9b408e0f7
User1 <Adminl@EMER00>
2018-07-09 17:56:09 +0200

    Ajout des classes métiers dans le DbContext et réinstallation d'entity FW

--- WebApplication1/WebApplication1/Data/DbContextBoVoyage.cs
+++ WebApplication1/WebApplication1/Data/DbContextBoVoyage.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
-using TodoList.Models;
+using BoVoyage.Models;
 
 namespace TodoList.Data
 {
-    public class DbContextBoVoyage : DbContext
+    public class BoVoyageDbContext : DbContext
     {
-        public DbContext() : base("AJOUTER NOM BASE DE DONNEES") // Constructeur de la classe de base
+        public BoVoyageDbContext() : base("AJOUTER NOM BASE DE DONNEES") // Constructeur de la classe de base
         {
         }
 
         public DbSet<Client> Clients { get; set; }
 
         public DbSet<Voyage> Voyages { get; set; }
+
+        public DbSet<AgenceVoyage> AgenceVoyages { get; set; }
+
+        public DbSet<Assurance> Assurances { get; set; }
+
+        public DbSet<AssuranceAnnulation> AssuranceAnnulations { get; set; }
+
+        public DbSet<Destination> Destinations { get; set; }
+
+        public DbSet<DossiersReservation> DossiersReservations { get; set; }
+
+        public DbSet<Participant> Participants { get; set; }
+
+        public DbSet<Personne> Personnes { get; set; }
 
     }
 }
